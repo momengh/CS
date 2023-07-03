66478a975db23f038b8e322aad5d1a8d5be38eea
Momen Eneim <momeneneim@outlook.com>
2023-07-03 17:32:27 +0200

    Add type conversion methods, float to int32 and vice versa

--- Basic Level Practice/Program.cs
+++ Basic Level Practice/Program.cs
@@ -1,11 +1,31 @@
-﻿namespace Basic_Level_Practice
+﻿using System;
+
+namespace Basic_Level_Practice
 {
     class Basics
     {
         static void Main(string[] args)
         {
-            // Display the number of command line arguments.
-            Console.WriteLine(args.Length);
+            Console.WriteLine("Please enter a float number:");
+            float floatNumber = float.Parse(Console.ReadLine());
+
+            if (floatNumber != null) 
+            {
+                Console.WriteLine(TypeCoversion.ConvertFloatToInt32(floatNumber));
+            }
+            else
+                Console.WriteLine("Please enter a float number: ");
+
+
+            Console.WriteLine("Please enter an integer number:");
+            Int32 intNumber = Int32.Parse(Console.ReadLine());
+
+            if (intNumber != null)
+            {
+                Console.WriteLine(TypeCoversion.ConvertInt32ToFloat(intNumber));
+            }
+            else
+                Console.WriteLine("Please enter an integer number: ");
         }
     }
 }
---
+++ Basic Level Practice/TypeCoversion.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Level_Practice
+{
+    internal class TypeCoversion
+    {
+        public static int ConvertFloatToInt32(float number1) {
+            try
+            {
+                return Convert.ToInt32(number1);
+            }
+            catch (Exception message)
+            {
+                Console.WriteLine("Could not covert Float to Int32 - " + message);
+                return -1;
+            }
+        }
+
+        public static float ConvertInt32ToFloat(Int32 number1) 
+        {
+            try
+            {
+                return (float) number1;
+            }
+            catch (Exception message)
+            {
+                Console.WriteLine("Could not covert Int32 to float - " + message);
+                return -1;
+            }
+        }
+    }
+}
